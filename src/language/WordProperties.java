d3bef4c83642b71e85d28e131e2a28c93cbafd3c
Oliver Abdulrahim <oliver.abdulrahim@gmail.com>
2015-09-13 20:03:17 -0500

    General cleanup of WordProperties

--- src/language/WordProperties.java
+++ src/language/WordProperties.java
@@ -11,38 +11,7 @@ import java.util.EnumSet;
  */
 public enum WordProperties {
     
-// Grammatical properties
-    
-    /**
-     * Defines a {@code Word} that is a proper noun.
-     */
-    PROPER_NOUN,
-    
-    /**
-     * Defines a {@code Word} that is the first in its sentence. This has the 
-     * same effect as the {@link #PROPER_NOUN} property.
-     * 
-     * @see #PROPER_NOUN
-     */
-    LEADING_WORD,
-    
-    /**
-     * Defines a {@code Word} that is the last in its sentence.
-     */
-    TRAILING_WORD,
-    
-    /**
-     * Defines a {@code Word} that is followed by a comma or another form of
-     * punctuation other than "{@code .!?}".
-     */
-    DELINEATED_WORD,
-    
 // Difficulty properties    
-    
-    /**
-     * Defines a {@code Word} with default difficulty.
-     */
-    DEFAULT_DIFFICULTY,
     
     /**
      * Defines a {@code Word} with easy difficulty.
@@ -62,7 +31,7 @@ public enum WordProperties {
     /**
      * Provides for a set of all properties enumerated in this class.
      */
-    public static final EnumSet<WordProperties> PROPERTIES = 
+    public static final EnumSet<WordProperties> ALL = 
             EnumSet.allOf(WordProperties.class);
     
 }
