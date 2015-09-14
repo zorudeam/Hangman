512c308271954f548f55f72564fc403990e48c20
Oliver Abdulrahim <oliver.abdulrahim@gmail.com>
2015-09-14 18:20:02 -0500

    Refactored class methods for jar-safety and clarity.

--- src/game/Actor.java
+++ src/game/Actor.java
@@ -1,101 +1,98 @@
 package game;
 
+import java.net.URL;
 import java.util.Arrays;
 import javax.swing.ImageIcon;
 
 /**
  * The {@code Actor} enumeration is a structure for constant values relating to 
- * the "Hangman" game class. Each enumeration has a standard set of images with
- * pre-defined physical properties for use in a graphical user interface.
+ * images for a game of hangman. Each enumeration has a predefined set of images 
+ * for use in a graphical user interface.
  * 
  * @author Oliver Abdulrahim
  */
 public enum Actor {
     
     /**
-     * Enumeration that represents a classic Stick Figure. This enumeration
-     * should have {@code 8} images in total.
+     * Represents a classic Stick Figure. This enumeration should have {@code 8}
+     * images in total.
      */
-    HUMAN("Stick Figure", getImages("Human", 8)
-    ),
+    HUMAN("Stick Figure", getImages("Human", 8)),
     
     /**
-     * Enumeration that represents a Snowman. This enumeration should have 
-     * {@code 8} images in total.
+     * Represents a Snowman. This enumeration should have {@code 8} images in 
+     * total.
      */
-    SNOWMAN("Snowman", getImages("Snowman", 8)
-    );
+    SNOWMAN("Snowman", getImages("Snowman", 8));
     
     /**
-     * Field used for identifying the object and for ease of display purposes in
-     * graphical interfaces.
+     * Used for identifying the object and for display purposes in graphical 
+     * interfaces.
      */
-    private final String actorName;
+    private final String name;
     
     /**
-     * Field that contains this object's images for use in a graphical user
-     * display. This image array should be iterated in its entirety before the
-     * "end" of the Hangman game.
+     * Contains this object's images for use in a graphical interface. This 
+     * image array should be iterated in its entirety before the "loss state" of
+     * any individual hangman game.
      */
-    private final ImageIcon[] imageArray;
+    private final ImageIcon[] images;
     
     /**
-     * Accessor method for {@code actorName} field.
+     * Returns the name of this actor.
      *
-     * @return  Returns the {@code actorName} field of this instance.
+     * @return The name of this actor.
      */
     public String getActorName() {
-        return actorName;
+        return name;
     }
 
     /**
-     * Default and only constructor, instantiates all fields relating to the 
-     * {@code Actor} class with the given arguments.
+     * Creates an actor with the given arguments.
      * 
-     * @param name      The {@code String} to assign to {@code actorName}.
-     * @param images    The {@code ImageIcon[]} to assign to {@code imageArray}.     
+     * @param name The name of this actor.
+     * @param images The images associated with this actor.     
      */
     private Actor(String name, ImageIcon[] images) {
-        actorName  = name;
-        imageArray = images;
+        this.name = name;
+        this.images = images;
     }
     
     /**
-     * Method that generates an array of images using the given enumeration's 
-     * {@code ImageIcon} icon sets. These sets are located within the 
-     * "resources" folder of the project.
-     * 
-     * @param enumName      The name of the enumeration to retrieve the images 
-     *                      of.
-     * @param imageCount    The amount of images that are stored for the
-     *                      specified enumeration.
-     * @return              
+     * Returns a copy of the array of this actor's images.
+     *
+     * @return a copy of the array of this actor's images.
      */
-    public static final ImageIcon[] getImages(String enumName, int imageCount) {
+    public ImageIcon[] getImageArray() {
+        return Arrays.copyOf(images, images.length);
+    }
+    
+    /**
+     * Generates and returns an array of images using the given enumeration's 
+     * {@code ImageIcon} icon sets.
+     * 
+     * @param name The name of the enumeration whose images to retrieve.
+     * @param imageCount The amount of images that are stored for the specified 
+     *        enumeration.
+     * @return An array containing the individual images of the given enum.
+     */
+    public static final ImageIcon[] getImages(String name, int imageCount) {
         ImageIcon[] images = new ImageIcon[imageCount];
         for (int i = 0; i < images.length; i++) {
-            images[i] = new ImageIcon("resources/actors/" + enumName + "/" + i
-                    + ".png");
+            URL path = Actor.class.getResource("/resources/actors/" 
+                    + name + '/' + i + ".png");
+            images[i] = new ImageIcon(path);
         }
         return images;
     }
     
     /**
-     * Accessor method for {@code imageArray} field.
-     *
-     * @return  Returns the {@code imageArray} field of this instance.
-     */
-    public ImageIcon[] getImageArray() {
-        return Arrays.copyOf(imageArray, imageArray.length);
-    }
-    
-    /**
-     * Method that generates a {@code String} array of the names of every
-     * enumerated type in this {@code enum}.
+     * Generates a {@code String} array of the names of every enumerated type in
+     * this class.
      * 
-     * @return  A {@code String} array of names.
+     * @return A {@code String} array of the names of every enumerated type.
      */
-    public static final String[] getNames() {
+    public static final String[] allNames() {
         String[] names = new String[Actor.values().length];
         for (int i = 0; i < names.length; i++) {
             names[i] = Actor.values()[i].getActorName();
