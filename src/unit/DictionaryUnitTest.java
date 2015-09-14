39d82f53a09949d30026296213ee08013252fe58
Oliver Abdulrahim <oliver.abdulrahim@gmail.com>
2015-09-13 20:27:52 -0500

    Refactored for reusability

--- src/unit/DictionaryUnitTest.java
+++ src/unit/DictionaryUnitTest.java
@@ -15,12 +15,14 @@ import language.WordProperties;
 public class DictionaryUnitTest {
     
     /**
-     * Stores the default testing file. Contains 234371 words - good for 
-     * distribution testing.
+     * Stores the default testing file. Contains 234,371 words - good for 
+     * difficulty distribution testing.
      */
     static final File TARGET_FILE = new File("resources/dictionary.txt");
     
     private int easyCount = 0, mediumCount = 0, hardCount = 0;
+    
+    private Date start, end;
     
     /**
      * The test subject!
@@ -28,15 +30,19 @@ public class DictionaryUnitTest {
     private Dictionary dictionary;
     
     /**
-     * Tests the instantiation process of the dictionary. Prints out each key in
-     * the structure unless {@code quietMode} is {@code true}.
-     * 
-     * @param quietMode 
+     * Tests the instantiation process of the dictionary.
      */
-    void instantiate(boolean quietMode) {
+    void instantiate() {
+        dictionary = new Dictionary(TARGET_FILE);
+    }
+    
+    /**
+     * Counts each word in the dictionary. Prints out each key in the structure 
+     * unless {@code quietMode} is {@code true}.
+     */
+    void countWords(boolean quietMode) {
         System.out.println("Working...");
-        dictionary = new Dictionary(TARGET_FILE);    
-        Date start = new Date();
+        start = new Date();
         for (Map.Entry<Word, WordProperties> entry : dictionary.entrySet()) {
             if (Dictionary.isEasyWord(entry.getKey())) {
                 easyCount++;
@@ -51,18 +57,14 @@ public class DictionaryUnitTest {
                 System.out.println(entry.getKey() + "=" + entry.getValue());
             }
         }
+        end = new Date();
         System.out.println("Done!");
-        printResults(start, new Date());
-        reset();
     }
     
     /**
-     * Prints the results of the unit test using the given dates.
-     * 
-     * @param start The start time of the test.
-     * @param end The end time of the test.
+     * Prints the results of the unit test.
      */
-    void printResults(Date start, Date end) {
+    void printResults() {
         System.out.println("\n\n                      __...--~~~~~-._   _.-~~~~~--...__\n" +
                            "                    //               `V'               \\\\ \n" +
                            "                   //    Dictionary   |  Unit Test       \\\\ \n" +
@@ -77,17 +79,20 @@ public class DictionaryUnitTest {
         System.out.println("Medium count..........: " + mediumCount);
         System.out.println("Hard count............: " + hardCount);
         int sum = easyCount + mediumCount + hardCount;
-        System.out.println("Target/Actual total...: " + dictionary.entrySet().size() + '/' + sum);
+        System.out.println("Expected/Actual total...: " + dictionary.entrySet().size() + '/' + sum);
     }
     
     void reset() {
         easyCount = mediumCount = hardCount = 0;
-        dictionary = null;
+        start = end = null;
     }
     
     public static void main(String[] args) {
         DictionaryUnitTest test = new DictionaryUnitTest();
-        test.instantiate(false);
+        test.instantiate();
+        test.countWords(false);
+        test.printResults();
+//        test.reset();
     }
     
 }
