aabc4edd72bdfa3ba8c0a8c30aa1798a5f043008
Oliver Abdulrahim <oliver.abdulrahim@gmail.com>
2015-09-13 21:10:11 -0500

    Complete overhaul of word categorization. Now using collection that maps Word keys to WordProperties values; also added new difficulty sorting algorithm with tuneable variables

--- src/language/Dictionary.java
+++ src/language/Dictionary.java
@@ -3,21 +3,23 @@
  */
 package language;
 
-//<editor-fold defaultstate="collapsed" desc="Imports">
-
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.Collections;
-import java.util.Comparator;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 import java.util.Scanner;
-import utilities.collections.LinkedList;
-
-//</editor-fold>
+import java.util.Set;
+import java.util.TreeMap;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import utilities.functions.Utilities;
 
 /**
  * The {@code Dictionary} class stores all parseable tokens from a given file 
  * and stores them in a dictionary structure. Various operations can be 
- * performed on this list, such as finding the shortest token or the amount of 
+ * performed on this map, such as finding the shortest token or the amount of 
  * vowels in any particular string of characters. Additionally, items may be 
  * added or removed at any time.
  * 
@@ -29,124 +31,239 @@ import utilities.collections.LinkedList;
 public final class Dictionary {
     
     /**
-     * Contains this object's word dictionary.
+     * Contains this object's word dictionary. This map is sorted by word 
+     * difficulty
      */
-    private final LinkedList<Word> list;
+    private final Map<Word, WordProperties> words;
     
     /**
-     * Instantiates a new, empty {@code Dictionary} with no words in its list.
+     * Instantiates a new, empty {@code Dictionary} with no words in its map.
      */
     public Dictionary() {
         this(null);
     }
     
     /**
-     * Instantiates a new {@code Dictionary} and fills this object's 
-     * list with the contents of the specified {@code File}. Each token in the 
-     * file is parsed and placed into the list in a line-by-line basis.
+     * Instantiates a new {@code Dictionary} and fills this object's map with 
+     * the contents of the specified {@code File}. Each token in the file is 
+     * parsed and placed into the map in a line-by-line basis.
      *
-     * @param target The {@code File} to read into this object's list.
+     * @param target The {@code File} to read into this object's map.
      */
     public Dictionary(File target) {
-        list = new LinkedList<>();
+        words = new TreeMap<>();
         if (target != null) {
             constructDictionary(target);
-            sort();
         }
     }
     
     /**
-     * Each token in the given file is parsed and placed into the list in a 
+     * Each token in the given file is parsed and placed into the map in a 
      * line-by-line basis.
      *
-     * @param target The {@code File} to read into this object's list.
+     * @param target The {@code File} to read into this object's map.
      */
     private void constructDictionary(File target) {
         try {
             Scanner input = new Scanner(target.getAbsoluteFile());
             while(input.hasNext()) {
-                list.add(Word.constructWord(input.nextLine()));
+                Word w = new Word(input.nextLine());
+                words.put(w, judgeDifficulty(w));
             }
         } 
         catch (FileNotFoundException ex) {
-            ex.printStackTrace();
+            Logger.getLogger(Dictionary.class.getName()).log(Level.SEVERE, 
+                    "Could not find file: " + target, ex);
         }
     }
     
     /**
-     * Sorts this object's list in ascending order.
-     */
-    public void sort() {
-        Comparator<Word> c = (w1, w2) -> {
-            return w1.compareTo(w2);
-        };
-        Collections.sort(list, c);
-    }
-    
-    /**
-     * Adds a given {@code Word} into this object in forward alphabetical order.
-     * If the given {@code Word} already exists in the list, this method does 
-     * nothing.
+     * Returns an enumerated property depending on the difficulty of the given 
+     * {@code Word}.
      * 
-     * @param element The {@code String} to add.
+     * @param w The word to judge the difficulty of.
+     * @return An enumerated property depending on the difficulty of the given 
+     *         {@code Word}.
      */
-    public void add(Word element) {
-        if (!list.contains(element)) {
-            list.add(element);
-            sort();
+    public static WordProperties judgeDifficulty(Word w) {
+        if (isEasyWord(w)) {
+            return WordProperties.EASY_WORD;
+        }
+        else if (isMediumWord(w)) {
+            return WordProperties.MEDIUM_WORD;
+        }
+        else {
+            return WordProperties.HARD_WORD;
         }
     }
     
+// Difficulty tuning variables
+
     /**
-     * Removes an the item at the specified index in this object's list.
-     * 
-     * @param index The index value of the object to remove.
+     * Minimum amount of vowels for an easy word.
      */
-    public void remove(int index) {
-        list.remove(index);
+    private static final int EASY_VOWEL_THRESHOLD = 4;
+    
+    /**
+     * Minimum length for an easy word.
+     */
+    private static final int EASY_LENGTH_THRESHOLD = 7;
+    
+    /**
+     * Minimum amount of vowels for a medium word.
+     */
+    private static final int MEDIUM_VOWEL_THRESHOLD = 3;
+    
+    /**
+     * Minimum length for a medium word.
+     */
+    private static final int MEDIUM_LENGTH_THRESHOLD = 5;
+    
+    /**
+     * Determines if a given word is "easy" in difficulty. Words that are longer
+     * in length and have more vowels are generally considered easy words.
+     * 
+     * @param w The {@code Word} to test for difficulty.
+     * @return {@code true} if the given word is considered to be "easy" in
+     *         difficulty.
+     */
+    public static boolean isEasyWord(Word w) {
+        return w.vowelCount() >= EASY_VOWEL_THRESHOLD
+                && w.length() >= EASY_LENGTH_THRESHOLD;
     }
     
     /**
-     * Finds and returns the {@code Word} in this object's list with the 
-     * shortest length.
+     * Determines if a given word is "medium" in difficulty.
      * 
-     * @return The shortest {@code Word} in this object's list.
+     * @param w The {@code Word} to test for difficulty.
+     * @return {@code true} if the given word is considered to be "medium" in
+     *         difficulty.
      */
-    public Word getShortest() {
-        Word shortest = list.get(0);
-        for (int i = 1; i < list.size(); i++) {
-            if (shortest.compareTo(list.get(i)) < 0) {
-                shortest = list.get(i);
+    public static boolean isMediumWord(Word w) {
+        return w.vowelCount() >= MEDIUM_VOWEL_THRESHOLD
+                && w.length() >= MEDIUM_LENGTH_THRESHOLD;
+    }
+    
+    /**
+     * Determines if a given word is "hard" in difficulty. Words that are short
+     * in length and contain mostly consonants are generally considered hard 
+     * words.
+     * 
+     * @param w The {@code String} to test for difficulty.
+     * @return {@code true} if the given word is considered to be "hard" in
+     *         difficulty.
+     */
+    public static boolean isHardWord(Word w) {
+        return !isEasyWord(w) && !isMediumWord(w);
+    }
+    
+// Collection operations
+    
+    /**
+     * Adds a given {@code Word} into this object. If the given {@code Word} 
+     * already exists in the map, this method does nothing.
+     * 
+     * @param w The {@code Word} to add.
+     * @throws NullPointerException If the given word is {@code null}.
+     */
+    public void add(Word w) {
+        Objects.requireNonNull(w);
+        words.put(w, judgeDifficulty(w));
+    }
+    
+    /**
+     * Removes the specified word from this object's map.
+     * 
+     * @param w The object to remove.
+     */
+    public void remove(Word w) {
+        words.remove(w);
+    }
+    
+    /**
+     * Returns a random {@code Word} from this object's {@code map}.
+     * 
+     * @return A random {@code String} from this object's {@code map}.
+     */
+    public Word getAnyWord() {
+        List<Word> list = new ArrayList<>(words.keySet());
+        return list.get(Utilities.r.nextInt(words.size()));
+    }
+    
+    /**
+     * Returns a randomly selected word of the given difficulty from this 
+     * object's list.
+     * 
+     * @param difficulty The difficulty of the word to return.
+     * @return A randomly selected word of the given difficulty.
+     */
+    protected Word getWordOf(WordProperties difficulty) {
+        List<Word> list = new ArrayList<>();
+        words.forEach((Word key, WordProperties value) -> {
+            if (value == difficulty) {
+                list.add(key);
             }
-        }
-        return shortest;
+        });
+        return list.get(Utilities.r.nextInt(list.size()));
     }
     
     /**
-     * Finds and returns the {@code Word} in this object's list with the 
-     * longest length.
+     * Returns a randomly selected word of easy difficulty mapped to this 
+     * object.
      * 
-     * @return The longest {@code Word} in this object's list.
+     * @return A randomly selected word of easy difficulty.
      */
-    public Word getLongest() {
-        Word longest = list.get(0);
-        for (int i = 1; i < list.size(); i++) {
-            if (longest.compareTo(list.get(i)) > 0) {
-                longest = list.get(i);
-            }
-        }
-        return longest;
+    public Word getEasyWord() {
+        return getWordOf(WordProperties.EASY_WORD);
     }
     
     /**
-     * Returns a formatted {@code String} containing this object's list.
+     * Returns a randomly selected word of medium difficulty mapped to this 
+     * object.
      * 
-     * @return A formatted {@code String} containing this object's list.
+     * @return A randomly selected word of medium difficulty.
+     */
+    public Word getMediumWord() {
+        return getWordOf(WordProperties.MEDIUM_WORD);
+    }
+    
+    /**
+     * Returns a randomly selected word of hard difficulty mapped to this 
+     * object.
+     * 
+     * @return A randomly selected word of hard difficulty.
+     */
+    public Word getHardWord() {
+        return getWordOf(WordProperties.HARD_WORD);
+    }
+    
+    /**
+     * Returns a set containing all entries mapped to this object.
+     * 
+     * @return A set of all entries in this object.
+     */
+    public Set<Map.Entry<Word, WordProperties>> entrySet() {
+        return words.entrySet();
+    }
+    
+    /**
+     * Returns a set containing all keys mapped to this object.
+     * 
+     * @return A set of all keys in this object.
+     */
+    public Set<Word> keySet() {
+        return words.keySet();
+    }
+    
+    /**
+     * Returns a formatted {@code String} containing this object's map.
+     * 
+     * @return A formatted {@code String} containing this object's map.
      */
     @Override
     public String toString() {
         return "Dictionary{"
-                + "list = " + list
+                + "map = " + words
                 + '}';
     }
 
